4fc127ad105b20b17f4acb9e98ba1bef7759f3dd
Riley <tudbury@gmail.com>
2020-02-15 13:10:40 -0700

    todo formatting

--- app/models/todo.js
+++ app/models/todo.js
@@ -10,16 +10,16 @@ export default class Todo {
   get todoTemplate() {
     return `
   
-    <li class="list-group-item bg-dark text-light"> ${this.Button}   ${this.description} <button class="ml-auto btn btn-sm btn-danger" onclick="app.todoController.removeTodo('${this._id}')"><i class="fas fa-trash-alt"></i></button></li>
+    <li class="list-group-item bg-dark text-light text-center"> ${this.Button}   ${this.description} <button class="no-bd ml-auto btn btn-sm btn-danger" onclick="app.todoController.removeTodo('${this._id}')"><i class="fas fa-trash-alt"></i></button></li>
   
   `
   }
 
   get Button() {
     if (this.completed == true) {
-      return `<button class="btn btn-sm btn-success" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="fas fa-check"></i></button>`
+      return `<button class="no-bd btn btn-sm btn-success" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="fas fa-check-square"></i></button>`
     }
-    return `<button class="btn btn-sm btn-secondary" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="fas fa-check"></i></button>`
+    return `<button class="no-bd btn btn-sm btn-secondary" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="far fa-check-square"></i></button>`
   }
 
 
