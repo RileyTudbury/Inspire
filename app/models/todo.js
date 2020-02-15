1ff1824b3c1be7bcdb417be9642b7cfac1332ee0
Riley <tudbury@gmail.com>
2020-02-15 13:15:29 -0700

    more todo formatting

--- app/models/todo.js
+++ app/models/todo.js
@@ -17,9 +17,9 @@ export default class Todo {
 
   get Button() {
     if (this.completed == true) {
-      return `<button class="no-bd btn btn-sm btn-success" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="fas fa-check-square"></i></button>`
+      return `<button class="no-bd btn btn-sm btn-success mr-2" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="far fa-check-square"></i></button>`
     }
-    return `<button class="no-bd btn btn-sm btn-secondary" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="far fa-check-square"></i></button>`
+    return `<button class="no-bd btn btn-sm btn-secondary mr-2" onclick="app.todoController.toggleTodoStatus('${this._id}')"><i class="far fa-square"></i></button>`
   }
 
 
