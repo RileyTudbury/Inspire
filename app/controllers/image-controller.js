16d62f5cd0882edeb95692d2b827043b69e2ebeb
Riley <tudbury@gmail.com>
2020-02-15 13:30:04 -0700

    remove todo

--- app/controllers/image-controller.js
+++ app/controllers/image-controller.js
@@ -2,10 +2,6 @@
 import store from "../store.js"
 import imageService from "../services/image-service.js";
 
-
-//TODO Create methods for constructor, and rendering the image to the page
-//      (you may wish to set it as a background image)
-
 function _drawBackground() {
 
   let image = store.State.image
